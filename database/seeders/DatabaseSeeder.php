2f0a1cf446b9bf5cf65a679e937dd3d1532eb34b
EmilianoMoreno13 <142268521+EmilianoMoreno13@users.noreply.github.com>
2023-10-05 00:46:43 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,15 +10,12 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        //
+        /*$this->call([
+            ProductSeeder::class,
+        ]);*/
+        $this->call(RolSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
