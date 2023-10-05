9da81a52a5c84bae8a9b4faa4ab236a19ab287fa
EmilianoMoreno13 <142268521+EmilianoMoreno13@users.noreply.github.com>
2023-10-05 00:48:31 -0300

    Update RolSeeder.php

--- database/seeders/RolSeeder.php
+++ database/seeders/RolSeeder.php
@@ -9,11 +9,12 @@ use Spatie\Permission\Models\Role;
 
 class RolSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run()
-    {
-        //
-    }
+        
+  public function run(): void{ $rol_admin = Role::create(['name'=>'admin']);
+                              $rol_vendedor = Role::create(['name'=>'vendedor']);
+                              $rol_cliente = Role::create(['name'=>'cliente']);
+                              Permission::create(['name'=>'lista_usuarios'])->assignRole($rol_admin);
+                              Permission::create(['name'=>'lista_productos'])->assignRole($rol_vendedor);
+                              Permission::create(['name'=>'lista_compras'])->assignRole($rol_cliente);
+  }
 }
