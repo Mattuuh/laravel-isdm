69bf774338fbaab2376d2b895f42be0d12f38b8a
EmilianoMoreno13 <142268521+EmilianoMoreno13@users.noreply.github.com>
2023-10-25 23:27:15 -0300

    Update panel.php

--- routes/panel.php
+++ routes/panel.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function (){
     return view('panel.index');
 
+});
+
+//Route::resource('/productos', ProductoController::class)->names('producto');
+
+Route::group(['middleware' => ['can:lista_productos']], function () {
+    Route::resource('/productos', ProductoController::class)->names('producto');
 });
